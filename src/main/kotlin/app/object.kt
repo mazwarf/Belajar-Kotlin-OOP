359b89156495ed8d12b212db247f16af7e5bf9a8
mazwarf <firmansyahazwar@gmail.com>
2024-01-18 21:10:58 +0700

    aman

--- src/main/kotlin/app/object.kt
+++ src/main/kotlin/app/object.kt
@@ -1,2 +1,18 @@
 package app
 
+import data.Person
+
+fun main() {
+    val firmansyah = Person()
+    firmansyah.firstName = "azwar"
+
+    val joko = Person()
+    joko.firstName = "Joko"
+
+    val azwar = Person()
+    azwar.firstName = "Firmansyah"
+
+    println(firmansyah.firstName)
+    println(joko.firstName)
+    println(azwar.firstName)
+}
